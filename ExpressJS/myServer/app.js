0ad78baea6345dee414b3f3cb49ae98ab8cdb050
Pascal Masny <pascal.masny@protonmail.com>
2022-12-20 14:41:41 +0100

    add aufgabe 4 und 5

--- ExpressJS/myServer/app.js
+++ ExpressJS/myServer/app.js
@@ -2,13 +2,19 @@ const express = require('express');
 const app = express();
 const EXP_PORT = 8080;
 
-//app.get(/\/[^\/]{3}.*/, (req, res) => {
-  //  res.send("Anfrage mit mind. drei Zeichen bis zum 1. Slash");
-//});
+app.use(express.static('public'));
+app.use(express.urlencoded({extendet: true}));
 
-// Verarbeitung aller GET-Anfragen der Route /meineTestanfrage
+
+
+//02 Aufagbe 2
 app.get("/meineTestanfrage", (req, res) => {
     // Senden des Textes an den Client
+    res.send("Ich habe die Anfrage empfangen!");
+});
+
+//02 Aufgabe 3
+app.get("/meineTestanfrage*", (req, res) => {
     res.send("Ich habe die Anfrage empfangen!");
 });
 
@@ -22,9 +28,31 @@ app.get("/kunde/anfrage", (req, res) => {
     res.end();
 });
 
+// 02 Aufgabe 4
 app.get("/abteilung/:abteilungId/kunde/:kundeID", (req, res) => {
     const {abteilungId, kundeID} = req.params;
     res.send(`Abteilung: ${abteilungId}<br/>Kunde: ${kundeID}`);
+});
+
+//* ODER
+
+app.get("/abteilung/:abteilungId/kunde/:kundeID", (req, res) => {
+    const {kundeID, abteilungId} = req.params;
+    const anrede = req.query.anrede ? req.query.anrede : "";
+    console.log(req.query.anrede);
+    res.send(`Abteilung: ${abteilungId}<br/>Kunde: ${anrede} ${kundeID}`);
+});
+
+// 02 Aufgabe 4 testMessage Form 
+app.use(express.urlencoded());
+app.post('/testMessage', function (req, res, next) {
+    res.send("I received: " + req.body.myMessage);
+});
+
+// 02 Aufgabe 5
+app.use(express.urlencoded());
+app.post('/addnum', function (req, res, next) {
+    res.send("Solution: " +(parseInt(req.body.num1) + parseInt(req.body.num2)));
 });
 
 
@@ -32,6 +60,7 @@ app.get("/abteilung/:abteilungId/kunde/:kundeID", (req, res) => {
 
 
 
+//! Sever Main
 app.listen(EXP_PORT, () => {
     console.log("Ich höre auf Port! " + EXP_PORT);
 })
