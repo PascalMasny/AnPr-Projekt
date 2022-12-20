a5147cee3757279311c3155363ae33d8a65542de
Pascal Masny <pascal.masny@protonmail.com>
2022-12-20 13:00:30 +0100

    add aufgabe 4 und 5 anfang

--- ExpressJS/myServer/app.js
+++ ExpressJS/myServer/app.js
@@ -1,21 +1,37 @@
-// Laden der Funktion zur Erstellung eines Express Servers und Zuweisung
-// an eine Konstante "express"
-
 const express = require('express');
-
-// Erstellung des Express Servers und Zuweisung an eine Konstante "app"
 const app = express();
-
-// Festlegung des Ports in einer Konstanten für späteren Gebrauch
 const EXP_PORT = 8080;
 
-// Erlauben des statischen Zugriffs auf den Unterordner "public", indem
-// als Middleware die Methode "static" eingesetzt wird
-app.use(express.static('public'));
+//app.get(/\/[^\/]{3}.*/, (req, res) => {
+  //  res.send("Anfrage mit mind. drei Zeichen bis zum 1. Slash");
+//});
 
-// Starten des Servers und Übergabe einer Funktion über einen Lambda
-// Ausdruck, welche nach Start ausgeführt werden soll
+// Verarbeitung aller GET-Anfragen der Route /meineTestanfrage
+app.get("/meineTestanfrage", (req, res) => {
+    // Senden des Textes an den Client
+    res.send("Ich habe die Anfrage empfangen!");
+});
+
+app.get("/kunde/*", (req, res, next) => {
+    res.write("Kunde\n");
+    next();
+});
+    
+app.get("/kunde/anfrage", (req, res) => {
+    res.write("Kunde und Anfrage");
+    res.end();
+});
+
+app.get("/abteilung/:abteilungId/kunde/:kundeID", (req, res) => {
+    const {abteilungId, kundeID} = req.params;
+    res.send(`Abteilung: ${abteilungId}<br/>Kunde: ${kundeID}`);
+});
+
+
+
+
+
+
 app.listen(EXP_PORT, () => {
-    // Konsolenausgabe
-    console.log("Ich höre auf Port " + EXP_PORT);
+    console.log("Ich höre auf Port! " + EXP_PORT);
 })
